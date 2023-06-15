34c034f39fe95b494a67d84fe0f6c6bd9ff61ebd
KyleIvanT <KyleIvanTBarreras@yahoo.com>
2023-06-15 12:20:46 -0700

    Java Script

--- .vendia/indexs.js
+++ .vendia/indexs.js
@@ -6,18 +6,3 @@ const client = createVendiaClient({
   apiKey: 'FnrHMNU8pg4rDB2QLnd2ZnQdL2vULVncQqgvtfWvVrHk', // <---- API key
 });
 
-// Requesting a subset of fields on the Vendia Block type
-const listBlocksQuery = `
-  query listBlocks {
-    listVendia_BlockItems {
-      Vendia_BlockItems {
-        blockId
-      }
-      nextToken
-    }
-  }
-`
-const response = await client.request(listBlocksQuery)
-
-// Returns the full GraphQL "data" response
-console.log(response?.listVendia_BlockItems?.Vendia_BlockItems?.[0]?.blockId)
